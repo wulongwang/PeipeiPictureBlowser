a89c4f0f480f81e310fdef138a662132b8251321
wulongwang <fbi080703@sina.com>
2016-03-28 19:04:05 +0800

    Initial Commit

---
+++ PeipeiPictureBlowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeipeiPictureBlowser
+//
+//  Created by 58 on 16/3/28.
+//  Copyright © 2016年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PeipeiPictureBlowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeipeiPictureBlowser
+//
+//  Created by 58 on 16/3/28.
+//  Copyright © 2016年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
